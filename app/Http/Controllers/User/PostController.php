0a6279c82ee5c4e35f9a7cd2ea58eb8b4021b5e5
N01S3R <padlemen@gmail.com>
2022-02-17 08:24:35 +0100

    Add Bootstrap

--- app/Http/Controllers/User/PostController.php
+++ app/Http/Controllers/User/PostController.php
@@ -36,7 +36,7 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         $input = $request->all();
         Post::create($input);
