d492033404db4068bb7219cb2f41e1eae8910309
N01S3R <padlemen@gmail.com>
2022-02-20 20:39:38 +0100

    Fix Post Controller

--- app/Http/Controllers/User/PostController.php
+++ app/Http/Controllers/User/PostController.php
@@ -84,8 +84,8 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
     }
 }
